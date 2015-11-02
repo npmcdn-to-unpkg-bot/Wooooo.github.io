8b19b4d9fd8ce027edece48c7483828ffdb1cacf
Taewoo Kim <xodn4195+c9@gmail.com>
2015-11-02 10:01:13 +0000

    Deps for angular-g-recaptcha in main.js is added.

--- angular-g-recaptcha/scripts/main.js
+++ angular-g-recaptcha/scripts/main.js
@@ -7,7 +7,9 @@ requirejs.config({
         'angular':              '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-beta.1/angular.min',
         
         //angular unofficial libraries
-        'ui-router' :           '//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.min'
+        'ui-router' :           '//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.min',
+        
+        'angular-g-recaptcha' : 'angular-g-recaptcha'
     },
     
     shim: {
@@ -15,6 +17,9 @@ requirejs.config({
             exports: 'angular'
         },
         'ui-router': {
+            deps: ['angular']
+        },
+        'angular-g-recaptcha': {
             deps: ['angular']
         }
     }
