8de0c209f451cd21b3f50d388f17b413b35b6182
Taewoo Kim <xodn4195+c9@gmail.com>
2015-11-07 09:49:42 +0000

    New angular-g-recaptcha is applied.

--- angular-g-recaptcha/scripts/ctrl.js
+++ angular-g-recaptcha/scripts/ctrl.js
@@ -5,38 +5,21 @@ define(function(require){
     
     var ctrl = angular.module('ctrl', [])
     .controller('homeCtrl', function($grecaptcha, $document, $scope){
-        var target = angular.element($document[0].querySelector('.g-recaptcha'));
-        var _widgetId = undefined;
+        $scope.greInfo = {};
         
-        $scope.response = undefined;
-        
-        var callback = function(response){
-            $scope.response = response; 
-        };
-        
-        var expiredCallback = function(){
-            $scope.response = undefined;
-        }
-        
-        
-        $grecaptcha.init().then(function(){
-            target.empty();
-            
-            $grecaptcha.render(target[0], {callback: callback, 'expired-callback': expiredCallback}).then(function(widgetId){
-                _widgetId = widgetId;
-            })
-        })
-        
-        $scope.reset = function(){
-            $grecaptcha.reset(_widgetId);
-            $scope.response = undefined;
-        };
-        
-        $scope.getResponse = function(){
-            var res = $grecaptcha.getResponse(_widgetId);
-            
-            alert( res == '' ? 'There is no repsonse yet!' : res );
-        }
+        $scope.$on('greInfo', function(greInfo) {
+            if(!!greInfo) {
+                greInfo.promise.then(function(gre){
+                    $scope.reset = function() {
+                        gre.reset();
+                    }
+                    
+                    $scope.getResponse = function() {
+                        alert(gre.getResponse());
+                    }
+                });
+            }    
+        });
     });
     
     
