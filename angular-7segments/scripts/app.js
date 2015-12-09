2a870801acbb5031ab3f044bb710249583125dcc
Taewoo Kim <xodn4195+c9@gmail.com>
2015-12-09 02:28:37 +0000

    Update examples.

--- angular-7segments/scripts/app.js
+++ angular-7segments/scripts/app.js
@@ -14,7 +14,7 @@ var app = angular.module('segment', ['wo.7segments', 'hljs'])
             scope.cardBlock = 'partials/'+scope.cardName+'Eg.html';
             scope.cardFooter = 'partials/'+scope.cardName+'EgDes.html';
         }
-    };
+    }
     
     return directiveDefinitionObject;
 })
@@ -29,8 +29,16 @@ var app = angular.module('segment', ['wo.7segments', 'hljs'])
     $scope.value = '-_.123SEG';
     $scope.options = {size: 12, watch:true, align: 'right'};
 })
-.run(function($rootScope, $location, $window) {
-    $rootScope.$on('$locationChangeSuccess', function(event, newState) {
-        $window.ga('send', 'pageview', { page: $location.path() });
-    });
+.controller('fourthEgCtrl', function($scope){
+    $scope.value = '12345678';
+    $scope.options = {
+        size: 10, 
+        watch:true, 
+        align: 'right'
+    };
+    
+    $scope.digitOptions = {
+        onClass: 'custom-segment-on',
+        digitClass: 'custom-segment-digit segment-digit-common'
+    };
 });
